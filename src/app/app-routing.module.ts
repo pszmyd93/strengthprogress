d18f472fdecc00fa9bce8bd1bd123e2fc37614c0
Pawel Szmyd <pawel.szmyd93@gmail.com>
2017-08-04 17:48:53 +0200

    przed dodawaniem ćwiczeń

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,11 +4,15 @@ import { PanelTreningComponent } from './panel-trening/panel-trening.component';
 import { PanelPlanyComponent } from './panel-plany/panel-plany.component';
 import { KalendarzComponent } from './kalendarz/kalendarz.component';
 import { TreningComponent } from './treningi/trening/trening.component';
+import { PlanTreningowyComponent } from './plany/plan-treningowy/plan-treningowy.component';
 
 const routes: Routes = [
     {path: 'panel-trening', component: PanelTreningComponent},
-    {path: 'treningi/trening', redirectTo: 'treningi/trening', pathMatch: 'full'},
+    {path: 'panel-trening/treningi/trening', component: TreningComponent},
+    {path: 'panel-plany/treningi/trening', component: TreningComponent},
+    {path: 'panel-plany/plany/plan-treningowy', component: PlanTreningowyComponent},
     {path: 'panel-plany', component: PanelPlanyComponent},
+    {path: 'panel-trening/panel-plany', redirectTo: 'panel-plany', pathMatch: 'full'},
     {path: 'kalendarz', component: KalendarzComponent},
     {path: '', redirectTo: 'panel-trening', pathMatch: 'full'}
 ];
