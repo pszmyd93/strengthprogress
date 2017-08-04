c887cf5af5237aec63e14933b508d72d6e485bae
Pawel Szmyd <pawel.szmyd93@gmail.com>
2017-08-04 14:22:27 +0200

    04.08.2017 rano

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,9 +3,11 @@ import { RouterModule, Routes } from '@angular/router';
 import { PanelTreningComponent } from './panel-trening/panel-trening.component';
 import { PanelPlanyComponent } from './panel-plany/panel-plany.component';
 import { KalendarzComponent } from './kalendarz/kalendarz.component';
+import { TreningComponent } from './treningi/trening/trening.component';
 
 const routes: Routes = [
     {path: 'panel-trening', component: PanelTreningComponent},
+    {path: 'treningi/trening', redirectTo: 'treningi/trening', pathMatch: 'full'},
     {path: 'panel-plany', component: PanelPlanyComponent},
     {path: 'kalendarz', component: KalendarzComponent},
     {path: '', redirectTo: 'panel-trening', pathMatch: 'full'}
