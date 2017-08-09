9fb96cf88775cf8d2ed22f3054632a88d6c77099
Pawel Szmyd <pawel.szmyd93@gmail.com>
2017-08-09 17:16:51 +0200

    input w szablonie ćwiczeń - nie działa

--- src/app/plany/szablon-cwiczenia/szablon-cwiczenia.component.ts
+++ src/app/plany/szablon-cwiczenia/szablon-cwiczenia.component.ts
@@ -1,17 +1,21 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { SzablonCwiczenia } from '../szablon-cwiczenia';
+import { SzablonCwiczeniaFormValues } from '../szablon-cwiczenia-form-values';
 
 @Component({
   selector: 'app-szablon-cwiczenia',
   templateUrl: './szablon-cwiczenia.component.html',
   styleUrls: ['./szablon-cwiczenia.component.css']
 })
-export class SzablonCwiczeniaComponent implements OnInit {
+export class SzablonCwiczeniaComponent {
   @Input() szablonCwiczenia: SzablonCwiczenia;
   @Input() nr: number;
+  @Input() initialLiczbaSerii: number = 1;
   constructor() { }
 
-  ngOnInit() {
-  }
+  onSubmit(formValues: SzablonCwiczeniaFormValues) {
 
+    console.log(formValues);
+
+  }
 }
